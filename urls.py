1816b57581db93b5da4b6f800d680db5964498d9
Megan Taylor <mtaylor@megantaylor.org>
2011-08-24 14:34:06 -0500

    cleanup

--- urls.py
+++ urls.py
@@ -1,14 +1,17 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+# Uncomment the next two lines to enable the admin:
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^firetracker/$', 'firetracker.fires.views.index'),
-    (r'^firetracker/fire/(?P<un_id>[-\w]+)/(?P<address>[-\w]+)/$', 'firetracker.fires.views.fire'),
-    (r'^firetracker/person/(?P<un_id>[-\w]+)/(?P<slug>[-\w]+)/$', 'firetracker.fires.views.person'),
-    (r'^firetracker/admin/', include(admin.site.urls)),
+    # Examples:
+    # url(r'^$', 'firestarter.views.home', name='home'),
+    # url(r'^firestarter/', include('firestarter.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+#    url(r'^admin/', include(admin.site.urls)),
 )
